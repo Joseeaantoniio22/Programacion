fb8ae7dacfc25116a6f240e6e99d7dd80f6b36b1
Joseeaantoniio22 <jfergue055@g.educaand.es>
2025-01-23 21:07:06 +0100

    herencia

--- JAVA/ejercicios/TEMA 9/HERENCIAS/ejePizarra/src/GestorDispositivo.java
+++ JAVA/ejercicios/TEMA 9/HERENCIAS/ejePizarra/src/GestorDispositivo.java
@@ -4,7 +4,7 @@ public class GestorDispositivo {
     public static void main(String[] args) {
         Dispositivo[] misDispositivos = new Dispositivo[MAX];
         misDispositivos[0] = new Computadora("16Gb", "1TB", "i7 12400F", "HP", true, "notepad");
-
+        misDispositivos[1] = new Televisor("Canal Sur", "Oled", false, 34, "Toshiba");
         for (int i = 0; i < Dispositivo.getContadorDisp(); i++) {
             ((Encendible) misDispositivos[i]).apagar();
             System.out.println(misDispositivos[i]);
--- JAVA/ejercicios/TEMA 9/HERENCIAS/ejePizarra/src/Televisor.java
+++ JAVA/ejercicios/TEMA 9/HERENCIAS/ejePizarra/src/Televisor.java
@@ -1,3 +1,31 @@
-public class Televisor {
-    
+public class Televisor extends Dispositivo implements Encendible {
+    private int diagonal;
+    private String tecnologia;
+    private boolean encendido;
+    private String canal;
+
+    Televisor(String canal, String tecnologia, boolean encendido, int diagonal, String marca){
+        super(marca);
+        this.tecnologia=tecnologia;
+        this.canal=canal;
+        this.diagonal=diagonal;
+        encendido=false;
+    }
+
+    public void encender(){
+        encendido=true;
+        canal= canal;
+    }
+
+    public void apagar(){
+        encendido=false;
+    }
+
+    @Override
+    public String toString(){
+        if (encendido==true) {
+            return "La tele esta encendida, está en el canal "+canal+", tu tele es de marca "+marca;
+        }
+        return "La tele está apagada y es de marca "+marca;
+    }
 }
