ac0d425a1be5d0a401f733f01736d3bd053858ed
Joseeaantoniio22 <jfergue055@g.educaand.es>
2025-02-17 21:05:09 +0100

    ejercicios

--- JAVA/ejercicios/TEMA 12/pizarra/src/DreamTeam.java
+++ JAVA/ejercicios/TEMA 12/pizarra/src/DreamTeam.java
@@ -9,18 +9,18 @@ public class DreamTeam {
     private static String URL = "jdbc:mariadb://localhost:3306/baloncesto";
     private static String USUARIO = "root";
     private static String CONTRASEÑA = "";
+    private static ConnectionPool miConnection = new ConnectionPool(URL, USUARIO, CONTRASEÑA);
+    private static GestorSocios miGestor = new GestorSocios(miConnection.getConnections());
     public static void main(String[] args) throws SQLException {
-        ConnectionPool miConnection = new ConnectionPool(URL, USUARIO, CONTRASEÑA);
-        GestorSocios miGestor = new GestorSocios(miConnection.getConnections());
-        miGestor.create(añadirSocio());
-        miGestor.delete(bajaSocio());
+
+
     }
 
     public static int leerOpcion(){
         return 0;
     }
 
-    public static Socio añadirSocio(){
+    public static void añadirSocio(){
         System.out.println("Dime el nombre del socio que quieres añadir");
         String nombre = System.console().readLine();
         System.out.println("Dime la edad correspondiente a esa persona");
@@ -31,11 +31,17 @@ public class DreamTeam {
         String localidad = System.console().readLine();
         System.out.println("Y por último dime el identificador de la persona");
         int id = Integer.parseInt(System.console().readLine());
-        Socio miSocio = new Socio(edad, id, nombre, localidad, estatura);
-        return miSocio;
+        try {
+            if (miGestor.create(new Socio(edad, id, nombre, localidad, estatura))) System.out.println("Socio creado");
+            else{
+                System.out.println("Ha ocurrido algún error en la creacion del socio");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static Socio bajaSocio(){
+    public static void bajaSocio(){
         System.out.println("Dime el nombre del socio que quieres añadir");
         String nombre = System.console().readLine();
         System.out.println("Dime la edad correspondiente a esa persona");
@@ -46,16 +52,29 @@ public class DreamTeam {
         String localidad = System.console().readLine();
         System.out.println("Y por último dime el identificador de la persona");
         int id = Integer.parseInt(System.console().readLine());
-        Socio miSocio = new Socio(edad, id, nombre, localidad, estatura);
-        return miSocio;
+        try {
+            if (miGestor.delete(new Socio(edad, id, nombre, localidad, estatura))) System.out.println("Socio eliminado");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void modificarSocio(){
-
+        System.out.println("Dime el nombre del socio que quieres añadir");
+        String nombre = System.console().readLine();
+        System.out.println("Dime la edad correspondiente a esa persona");
+        int edad = Integer.parseInt(System.console().readLine());
+        System.out.println("Dime la estatura de la persona");
+        int estatura = Integer.parseInt(System.console().readLine());
+        System.out.println("Dime la localidad de la persona");
+        String localidad = System.console().readLine();
+        System.out.println("Y por último dime el identificador de la persona");
+        int id = Integer.parseInt(System.console().readLine());
+        Socio miSocio = new Socio(edad, id, nombre, localidad, estatura);
     }
 
     public static void listarSocios(){
-
+        
     }
 
     public static void buscarSocio(){
